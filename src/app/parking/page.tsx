a96af67d04fe5f3b1fba3e6bfe169b026e71876f
suraj pro <71000909+suraj719@users.noreply.github.com>
2023-09-10 22:59:26 +0530

    fix: dynamic route

--- src/app/parking/page.tsx
+++ src/app/parking/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Page() {
   return (
-    <div>Parking page</div>
+    <div>Parking page
+      <Link href="/parking/1">click to go to page</Link>
+    </div>
   )
 }
